0b4bced5388adcdc52cea85723caa05670bc9677
Himanshu <himanshusharama.work@gmail.com>
2026-01-02 18:51:09 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+#Write a Python program that:
+# 1. 	Takes an integer input from the user.
+# 2. 	Checks whether the number is even or odd using an if-else statement.
+# 3. 	Displays the result accordingly.
+
+num_1 = int(input("Enter a number : "))
+
+
+if num_1 % 2 == 0:
+    print("Number is even")
+else :
+    print("Number is odd")
---
+++ Task_2.py
@@ -0,0 +1,11 @@
+#
+#  Write a Python program that:
+# 1.   Uses a for loop to iterate over numbers from 1 to 50.
+# 2.   Calculates the sum of all integers in this range.
+# 3.   Displays the final sum.
+
+
+j = 0
+for i  in range(1,51):
+    j += 1
+print(f"The sum of number (1 to 51) is {i+j}")
